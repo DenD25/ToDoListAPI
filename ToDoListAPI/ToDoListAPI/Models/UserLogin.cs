39547b745126a53fdba22f93fa05ecffb0bc6316
DenD <dendanyleiko@gmail.com>
2022-09-27 19:51:03 +0300

    Adding registration & login models

---
+++ ToDoListAPI/ToDoListAPI/Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAPI.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MaxLength(20)]
+        public string Password { get; set; }
+    }
+}
---
+++ ToDoListAPI/ToDoListAPI/Models/UserRegister.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAPI.Models
+{
+    public class UserRegister
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Role id is required")]
+        public int RoleId { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MaxLength(20)]
+        public string Password { get; set; }
+    }
+}
