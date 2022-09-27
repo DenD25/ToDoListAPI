c357d44f844345a53e6a190167f0b29faeae764b
DenD <dendanyleiko@gmail.com>
2022-09-27 21:56:13 +0300

    Adding services for password hashing

---
+++ ToDoListAPI/ToDoListAPI/Services/PasswordHash/IPasswordHash.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListAPI.Services.PasswordHash
+{
+    public interface IPasswordHash
+    {
+        public void CreateHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        public bool VerifyHash(string password, byte[] passwordHash, byte[] passwordSalt);
+    }
+}
---
+++ ToDoListAPI/ToDoListAPI/Services/PasswordHash/PasswordHash.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace ToDoListAPI.Services.PasswordHash
+{
+    public class PasswordHash: IPasswordHash
+    {
+        public void CreateHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public bool VerifyHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
