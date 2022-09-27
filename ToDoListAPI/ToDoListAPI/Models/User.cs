d5704af0ed76cebda87023e56aff61c7eef62f09
DenD <dendanyleiko@gmail.com>
2022-09-27 19:50:35 +0300

    Adding User model

---
+++ ToDoListAPI/ToDoListAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int RoleId  { get; set; }
+        public Role Role { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
