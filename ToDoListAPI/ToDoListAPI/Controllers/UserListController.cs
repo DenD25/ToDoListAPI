811e5bd22dcf4414c69a75c4029eba3e02206849
DenD <dendanyleiko@gmail.com>
2022-09-28 19:18:10 +0300

    Adding UserList controller

---
+++ ToDoListAPI/ToDoListAPI/Controllers/UserListController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoListAPI.Data;
+using ToDoListAPI.Models;
+
+namespace ToDoListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserListController : ControllerBase
+    {
+        private DataContext db;
+
+        public UserListController(DataContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> UserList()
+        {
+            List<User> users = db
+                .Users
+                .ToList();
+
+            return Ok(users);
+        }
+    }
+}
